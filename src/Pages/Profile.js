6546b7ed6e3372455f137df616b76be4b2c83a52
tanmichael1 <michaelrstan@gmail.com>
2021-03-26 17:21:10 +1300

    Show user's created quizzes in profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -4,14 +4,24 @@ function Profile() {
   const [done, setDone] = useState(false);
   const [id, setId] = useState();
   const [username, setUsername] = useState("");
-  let createdQuizzes = [];
+  const [createdQuizzes, setCreatedQuizzes] = useState([]);
   let takenQuizzes = [];
   function setup() {
     firebase.auth().onAuthStateChanged((user) => {
       setId(user.uid);
       const dbRefUsers = firebase.database().ref(`Users/${user.uid}`);
+      const dbCreatedQuizzes = dbRefUsers.child("createdQuizzes");
       dbRefUsers.on("value", (snap) => {
         setUsername(snap.val().username);
+        if (snap.val().createdQuizzes == undefined) {
+        } else {
+          dbCreatedQuizzes.on("value", (snap) =>
+            snap.forEach((childSnapshot) => {
+              var tempArray = createdQuizzes;
+              setCreatedQuizzes(tempArray.push(childSnapshot.val().title));
+            })
+          );
+        }
       });
     });
 
@@ -25,7 +35,11 @@ function Profile() {
       <h1>{username}</h1>
 
       <h2>Created quizzes</h2>
-      {createdQuizzes > 0 ? <div>Test</div> : <div>Empty</div>}
+      {createdQuizzes > 0 ? (
+        <div>Test</div>
+      ) : (
+        <div>Not created any quizzes</div>
+      )}
 
       <h2>Recently taken quizzes</h2>
       {takenQuizzes > 0 ? <div>Test</div> : <div>Taken no quizzes</div>}
